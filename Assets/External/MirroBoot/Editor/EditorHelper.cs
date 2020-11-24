bb2ae3cc018bcc40afc92a2895cb491fabb09ecd
tkaria <tkaria.info@gmail.com>
2020-11-24 13:20:06 +0900

    apply windows

--- Assets/External/MirroBoot/Editor/EditorHelper.cs
+++ Assets/External/MirroBoot/Editor/EditorHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
@@ -11,12 +12,33 @@ namespace AriaSDK.MirrorBoot.Editor
 	/// </summary>
 	internal static class EditorHelper
 	{
+#if UNITY_EDITOR_WIN
+		const string FILE_NAME = "setupSynchronzableProject.bat";
+#else // UNITY_EDITOR_OSX
 		const string FILE_NAME = "setupSynchronzableProject.sh";
+#endif 
 
+		private static string GetScriptPath()
+		{
+			var results = AssetDatabase.FindAssets($"")
+				.Select(x => AssetDatabase.GUIDToAssetPath(x))
+				.Where(x => x.Contains($"{FILE_NAME}"))
+				.ToList();
+
+			return (results.Count > 0) ? results[0].Replace($"{FILE_NAME}", ""): null;
+		}
 
 		[UnityEditor.MenuItem("AriaSDK/Tools/GenerateSynchronizableProject")]
 		private static void GenerateSynchronizableProject()
 		{
+			var path = GetScriptPath();
+			if (string.IsNullOrEmpty(path))
+			{
+				return;
+			}
+
+			Debug.Log(path);
+
 			var runner = new BatchRunner($"{Application.dataPath}/External/MirroBoot/", $"{FILE_NAME}");
 			string log = runner.Run();
 			Debug.Log(log);
