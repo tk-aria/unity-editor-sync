7ca61a0f8af67b7219868d70df10c6780f317fe9
tkaria <tkaria.info@gmail.com>
2020-11-27 01:43:14 +0900

    fix fetch path on upm

--- Assets/External/Mirrorboot/Editor/EditorHelper.cs
+++ Assets/External/Mirrorboot/Editor/EditorHelper.cs
@@ -8,76 +8,84 @@ using UnityEditor;
 namespace AriaSDK.MirrorBoot.Editor
 {
 
-	/// <summary>
-	/// 
-	/// </summary>
-	internal static class EditorHelper
-	{
+    /// <summary>
+    ///
+    /// </summary>
+    internal static class EditorHelper
+    {
 #if UNITY_EDITOR_WIN
-		const string FILE_NAME = "setupSynchronizableProject.bat";
-#else // UNITY_EDITOR_OSX || 
-		const string FILE_NAME = "setupSynchronizableProject.sh";
-#endif 
+        const string FILE_EXTENSION = "bat";
+#else // UNITY_EDITOR_OSX ||
+        const string FILE_EXTENSION = "sh";
+#endif
+        const string FILE_NAME = "setupSynchronizableProject";
 
-		private static string GetScriptPath()
-		{
-			var results = AssetDatabase.FindAssets($"")
-				.Select(x => AssetDatabase.GUIDToAssetPath(x))
-				.Where(x => x.Contains($"{FILE_NAME}"))
-				.ToList();
+        const string MENU_PATH = "AriaSDK/Tools/GenerateSynchronizableProject";
 
-			return (results.Count > 0) ? results[0].Replace($"{FILE_NAME}", ""): null;
-		}
+        private static string GetScriptPath()
+        {
+            //var results = Directory.GetFiles("./", "*.sh", SearchOption.AllDirectories)
+            var results = Directory.GetFiles(Directory.GetCurrentDirectory(), $"*.{FILE_EXTENSION}", SearchOption.AllDirectories)
+                .Where(x => x.Contains($"{FILE_NAME}"))
+                .ToList();
 
-		struct UnityAppInfo
-		{
-			public string path;
-			public string exec;
-		}
+            //var results = AssetDatabase.FindAssets($"")
+            //	.Select(x => AssetDatabase.GUIDToAssetPath(x))
+            //	.Where(x => x.Contains($"{FILE_NAME}"))
+            //	//.Select(x => FileUtil.GetProjectRelativePath(x))
+            //	.ToList();
 
-		private static UnityAppInfo CreateUnityAppInfo()
-		{
-			string unityAppPath = $"{EditorApplication.applicationPath}";
+            return (results.Count > 0) ? results[0].Replace($"{FILE_NAME}.{FILE_EXTENSION}", ""): null;
+        }
 
-			return new UnityAppInfo{
-				path = Path.GetDirectoryName(unityAppPath),
-				exec = Path.GetFileName(unityAppPath)
-			#if UNITY_EDITOR_OSX
-					+ "/Contents/MacOS/Unity"
-			#endif
-			};
+        struct UnityAppInfo
+        {
+            public string path;
+            public string exec;
+        }
 
-		}
+        private static UnityAppInfo CreateUnityAppInfo()
+        {
+            string unityAppPath = $"{EditorApplication.applicationPath}";
 
-		[UnityEditor.MenuItem("AriaSDK/Tools/GenerateSynchronizableProject")]
-		private static void GenerateSynchronizableProject()
-		{
-			var path = GetScriptPath();
-			if (string.IsNullOrEmpty(path))
-			{
-				return;
-			}
+            return new UnityAppInfo{
+                path = Path.GetDirectoryName(unityAppPath),
+                exec = Path.GetFileName(unityAppPath)
+            #if UNITY_EDITOR_OSX
+                    + "/Contents/MacOS/Unity"
+            #endif
+            };
+        }
 
-			var shellRunner = new BatchRunner($"{path}", $"{FILE_NAME}");
-			{
-				string log = shellRunner.Run();
-				Debug.Log(log);
-			}
+        [UnityEditor.MenuItem(MENU_PATH)]
+        private static void GenerateSynchronizableProject()
+        {
+            var path = GetScriptPath();
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
 
-			var unityAppInfo = CreateUnityAppInfo();
+            var shellRunner = new BatchRunner($"{path}", $"{FILE_NAME}.{FILE_EXTENSION}");
+            {
+                string log = shellRunner.Run();
+                Debug.Log(log);
+            }
 
-			var unityCmdParam = new System.Diagnostics.ProcessStartInfo{
-				FileName = $"{unityAppInfo.path}/{unityAppInfo.exec}",
-				Arguments = $"-projectPath ../Synchronizable{Application.productName}"
-			};
-			System.Diagnostics.Process.Start(unityCmdParam);
+            var unityAppInfo = CreateUnityAppInfo();
 
-			// [todo] wait for exit.
-			//var unityCommandline = new BatchRunner(unityAppInfo.path, unityAppInfo.exec);
-			//{
-			//	string log = unityCommandline.Run($"-projectPath ../Synchronizable{Application.productName}");
-			//	Debug.Log(log);
-			//}
-		}
-	}
+            var unityCmdParam = new System.Diagnostics.ProcessStartInfo{
+                FileName = $"{unityAppInfo.path}/{unityAppInfo.exec}",
+                Arguments = $"-projectPath ../Synchronizable{Application.productName}"
+            };
+            System.Diagnostics.Process.Start(unityCmdParam);
+
+            // [todo] wait for exit.
+            //var unityCommandline = new BatchRunner(unityAppInfo.path, unityAppInfo.exec);
+            //{
+            //	string log = unityCommandline.Run($"-projectPath ../Synchronizable{Application.productName}");
+            //	Debug.Log(log);
+            //}
+        }
+    }
 }
